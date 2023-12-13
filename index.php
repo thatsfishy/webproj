1b6193a51a594da251269d08a335c4e734213c22
thatsfishy <81892530+thatsfishy@users.noreply.github.com>
2023-12-13 10:33:30 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfumes & Colognes Store</title>
+    <meta name="description" content="Explore a wide range of perfumes and colognes at our store.">
+    <meta name="keywords" content="perfumes, colognes, fragrance, scent, perfume shop">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+    <meta property="og:title" content="Perfumes & Colognes Store">
+    <meta property="og:description" content="Explore a wide range of perfumes and colognes at our store.">
+    <meta property="og:image" content="image-url">
+    <meta name="twitter:title" content="Perfumes & Colognes Store">
+    <meta name="twitter:description" content="Explore a wide range of perfumes and colognes at our store.">
+    <meta name="twitter:image" content="image-url">
+    <link rel="canonical" href="https://www.yourperfumestore.com">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            /* Additional styles for the body can be added here */
+        }
+    </style>
+</head>
+
+
+  <body>
+      <div class="navbar">
+          <div class="container">
+              <ul class="nav-links">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/products/">Products</a></li>
+                  <li><a href="/checkout/">Checkout</a></li>
+                  <li><a href="/contact/">Contact</a></li>
+              </ul>
+          </div>
+      </div>
+    <h1>
+      Cologns and Parfumes
+    </h1>
+  </body>
+
+
+</html>
